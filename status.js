0532a0d1057ab08d24b5f8cc447f81fdeba027df
potassium-platinums <59970163+potassium-platinums@users.noreply.github.com>
2025-07-27 06:52:55 +0300

    Update status.js

--- status.js
+++ status.js
@@ -33,7 +33,7 @@ mqttClient.on('message', (topic, message) => {
 
     const payload = {
       device_id: data.device_id,
-      status: "OFFLINE",
+      status: data.status,
     };
 
     axios.post('https://coffee.rndnakawa.com/api/get_status.php', payload, {
