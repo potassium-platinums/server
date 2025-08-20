df263036221efc12c3a0db2ea1fd004a3bb3afa8
potassium-platinums <59970163+potassium-platinums@users.noreply.github.com>
2025-08-20 07:57:13 +0300

    Update content.js

--- content.js
+++ content.js
@@ -60,43 +60,43 @@ mqttClient.on('message', (topic, message) => {
     });
 
 
-  function updateDateDisplay() {
-      const date = new Date();
-      const longDayName = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(date);
-    const graph = {
-      // device_id: data.device_id,
-      count: data.count,
-      date: data.date,
-    };
-      // get data from db
-    axios.post('https://coffee.rndnakawa.com/api/barGraph.php', graph, {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      // timeout: 5000
-    })
-    .then(response => {
-      console.log('Server response:', response.data);
-    })
-    .catch(error => {
-      if (error.response) {
-        console.error('Server responded with error:', error.response.status, error.response.data);
-      } else if (error.request) {
-        console.error('No response received:', error.message);
-      } else {
-        console.error('Error setting up request:', error.message);
-      }
-    });
-  }
+  // function updateDateDisplay() {
+  //     const date = new Date();
+  //     const longDayName = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(date);
+  //   const graph = {
+  //     // device_id: data.device_id,
+  //     count: data.count,
+  //     date: data.date,
+  //   };
+  //     // get data from db
+  //   axios.post('https://coffee.rndnakawa.com/api/barGraph.php', graph, {
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     // timeout: 5000
+  //   })
+  //   .then(response => {
+  //     console.log('Server response:', response.data);
+  //   })
+  //   .catch(error => {
+  //     if (error.response) {
+  //       console.error('Server responded with error:', error.response.status, error.response.data);
+  //     } else if (error.request) {
+  //       console.error('No response received:', error.message);
+  //     } else {
+  //       console.error('Error setting up request:', error.message);
+  //     }
+  //   });
+  // }
 
-  const now = new Date();
-  const midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0); // Next midnight
-  const timeUntilMidnight = midnight.getTime() - now.getTime();
+  // const now = new Date();
+  // const midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0); // Next midnight
+  // const timeUntilMidnight = midnight.getTime() - now.getTime();
 
-  setTimeout(() => {
-    updateDateDisplay();
-    setInterval(updateDateDisplay, 24*60*60*1000); //for a day
-  }, timeUntilMidnight); // midnight update
+  // setTimeout(() => {
+  //   updateDateDisplay();
+  //   setInterval(updateDateDisplay, 24*60*60*1000); //for a day
+  // }, timeUntilMidnight); // midnight update
 
 
 });
